40d9159092d84163d58f2e4e24175728f6139a47
blazobanicevic <blazoban777@gmail.com>
2026-07-06 01:01:01 +0200

    Designed user and admin dashboards

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,16 +1,20 @@
 using System.Diagnostics;
+using BookingMVC.Data;
 using BookingMVC.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace BookingMVC.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly BookingDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, BookingDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
@@ -34,26 +38,45 @@ namespace BookingMVC.Controllers
         {
             if (HttpContext.Session.GetString("Uloga") != "Korisnik")
             {
-                return RedirectToAction("AdminDashboard");
+                return RedirectToAction("Index");
             }
 
-            return View();
+            var smjestaji = _context.Smjestaji
+                .Include(s => s.Grad)
+                .Include(s => s.TipSmjestaja)
+                .Where(s => s.Aktivan)
+                .Take(6)
+                .ToList();
+
+            return View(smjestaji);
         }
 
         public IActionResult AdminDashboard()
         {
             if (HttpContext.Session.GetString("Uloga") != "Admin")
             {
-                return RedirectToAction("UserDashboard");
+                return RedirectToAction("Index");
             }
 
-            return View();
+            int idAdmin = HttpContext.Session.GetInt32("IdKorisnik").Value;
+
+            var smjestaji = _context.Smjestaji
+                .Include(s => s.Grad)
+                .Include(s => s.TipSmjestaja)
+                .Where(s => s.IdAdmin == idAdmin)
+                .Take(6)
+                .ToList();
+
+            return View(smjestaji);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View(new ErrorViewModel
+            {
+                RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier
+            });
         }
     }
 }
